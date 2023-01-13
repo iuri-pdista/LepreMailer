be7182016669941863e8f788f938a06091f236a1
iuri-pdista <correaiuri86@gmail.com>
2023-01-13 18:30:13 -0300

    Criacao de lista de cadastros integrada ao envio de emails

--- LepreMailer.py
+++ LepreMailer.py
@@ -1,3 +1,4 @@
+import email
 import os
 from email.message import EmailMessage
 import ssl
@@ -5,14 +6,28 @@ import smtplib
 
 # remetente = "financeiro.leprechauns.rugby@gmail.com"
 # senha = os.environ.get(LEPRESENHA)
-def manda_email():
+def manda_email(destinatario):
     email = EmailMessage()
-    email['From'], email['To'] = "correaiuri86@gmail.com", "iuricorrea05@gmail.com"
-    email.set_content("FALA PUTA")
+    email['From'], email['To'] = "correaiuri86@gmail.com", destinatario[1]
+    email['Subject'] = "Lembrete do pagamento da mensalidade"
+    email.set_content(f"""
+        Bom dia, {destinatario[0]}.
+         Gostaríamos, em nome do financeiro do lepre, de lembrá-lo de realizar o pagamento da sua mensalidade no valor de R$30
+    """)
     senha = "zyixpaabciohafkg"
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = ssl.create_default_context()) as smtp_con:
         smtp_con.login(email['From'], senha)
         smtp_con.sendmail(email['From'], email['To'], email.as_string())
 
-
-
+def coleta_emails(nomeDoArquivo):
+    with open(nomeDoArquivo, 'r') as cadastros:
+        return cria_lista_emails(cadastros.read().split("\n"))
+        
+def cria_lista_emails(cadastros):
+    pessoas = {}
+    for cadastro in cadastros:
+        if cadastro:
+            nome, email = cadastro.split(";")[0], cadastro.split(";")[1]
+            pessoas[nome] = email
+    return pessoas
+manda_email(["Iuri" , "iuricorrea05@gmail.com"])
